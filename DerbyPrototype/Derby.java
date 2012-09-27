ed1220bb9434eae78b156bb3a3fb0f8e9b124585
Mariano Luis Uymatiao <wizfactormail@gmail.com>
2012-09-27 20:09:49 +0800

    Derby Prototype has been updated to support Queries by Date. If other queries are needed, please let me know. I will try to upload a DerbyDriver for testing purposes.

--- DerbyPrototype/Derby.java
+++ DerbyPrototype/Derby.java
@@ -1,4 +1,5 @@
 import java.sql.Connection;
+import java.sql.DatabaseMetaData;
 import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
@@ -6,7 +7,6 @@ import java.sql.SQLException;
 import java.sql.Statement;
 
 import java.util.ArrayList;
-import java.util.Properties;
 
 public class Derby
 {
@@ -20,63 +20,39 @@ public class Derby
 	 * with a compiler for J2SE 5.0 or newer. We are not using generics
 	 * because we want the source to support J2SE 1.4.2 environments. */
     ArrayList statements = new ArrayList(); // list of Statements, PreparedStatements
+    PreparedStatement entryInsert = null;
+    PreparedStatement entryUpdate = null;
+    PreparedStatement entryDelete = null;
+    
     PreparedStatement eventInsert = null;
     PreparedStatement eventUpdate = null;
+    PreparedStatement eventDelete = null;
+    PreparedStatement eventQuery = null;
+    
     PreparedStatement taskInsert = null;
     PreparedStatement taskUpdate = null;
+    PreparedStatement taskDelete = null;
+    PreparedStatement taskQuery = null;
+    
     Statement s = null;
     ResultSet rs = null;
 
     //ARGS
     //embedded - Use embedded database (DEFAULT)
     //derbyclient - Use client model to connect to a server
-    public static void main(String[] args)
-    {
-        new Derby().go(args);
-        System.out.println("SimpleApp finished");
-    }
 
-    /**
-     * Starts the actual demo activities. This includes loading the correct
-     * JDBC driver, creating a database by making a connection to Derby,
-     * creating a table in the database, and inserting, updating and retrieving
-     * some data. Some of the retrieved data is then verified (compared) against
-     * the expected results. Finally, the table is deleted and, if the embedded
-     * framework is used, the database is shut down.
-     */
-    void go(String[] args)
-    {
-        /* parse the arguments to determine which framework is desired*/
-        parseArguments(args);
+    public Derby() {
 
         System.out.println("Derby starting in " + framework + " mode");
 
         /* load the desired JDBC driver */
         loadDriver();
-
-        /* We will be using Statement and PreparedStatement objects for
-         * executing SQL. These objects, as well as Connections and ResultSets,
-         * are resources that should be released explicitly after use, hence
-         * the try-catch-finally pattern used below.
-         * We are storing the Statement and Prepared statement object references
-         * in an array list for convenience.
-         */
         
         try
         {
             String dbName = "derbyDB"; // the name of the database
-
-            /*
-             * This connection specifies create=true in the connection URL to
-             * cause the database to be created when connecting for the first
-             * time. To remove the database, remove the directory derbyDB (the
-             * same as the database name) and its contents.
-             *
-             * The directory derbyDB will be created under the directory that
-             * the system property derby.system.home points to, or the current
-             * directory (user.dir) if derby.system.home is not set.
-             */
-             conn = DriverManager.getConnection(protocol + dbName
+      
+            conn = DriverManager.getConnection(protocol + dbName
                     + ";create=true");
 
             System.out.println("Connected to and created database " + dbName);
@@ -90,37 +66,44 @@ public class Derby
             s = conn.createStatement();
             statements.add(s);
             // We create a table...
-            s.execute("CREATE TABLE event(name varchar(255), description varchar(255), date varchar(255), time varchar(255), isAllDay boolean, endTime varchar(255), repeating smallint)");
-            System.out.println("Created table event");
             
-            s.execute("CREATE TABLE task(name varchar(255), description varchar(255), date varchar(255), time varchar(255), " +
-            		"status smallint, priority smallint)");
-            System.out.println("Created table task");
+            DatabaseMetaData dmd = conn.getMetaData();
+            ResultSet tables = dmd.getTables(null, null, null, new String[]{"event","task"});
+            
+            createTables();       	
+        	
+            entryInsert = conn.prepareStatement("INSERT INTO entry (name, description, date, time) VALUES (?, ?, ?, ?)", Statement.RETURN_GENERATED_KEYS);
+            statements.add(entryInsert);
+            
+            entryUpdate = conn.prepareStatement("UPDATE entry SET name=?, description=?, date=?, time=?", Statement.RETURN_GENERATED_KEYS);
+            statements.add(entryUpdate);
+            
+            entryDelete = conn.prepareStatement("DELETE FROM entry WHERE E_id=?");
+            statements.add(entryDelete);
 
-            eventInsert = conn.prepareStatement("insert into event values (?, ?, ?, ?, ?, ?, ?)");
+            eventInsert = conn.prepareStatement("INSERT INTO event (E_id, isAllDay, endTime, repeating) VALUES (?, ?, ?, ?, ?)");
             statements.add(eventInsert);
-
-            // Let's update some rows as well...
-            eventUpdate = conn.prepareStatement(
-                        "UPDATE event SET name=?, description=?, date=?, time=?, isAllDay=?, endTime=?, repeating=?  where name=?");
-            statements.add(eventUpdate);
             
-            taskInsert = conn.prepareStatement("insert into task values (?, ?, ?, ?, ?, ?)");
+            eventUpdate = conn.prepareStatement("UPDATE event SET isAllDay=?, endTime=?, repeating=?  where E_id=?");
+            statements.add(eventUpdate);       
+            
+            eventDelete = conn.prepareStatement("DELETE FROM event WHERE E_id=?");
+            statements.add(eventDelete);
+            
+            eventQuery = conn.prepareStatement("SELECT * FROM entry LEFT JOIN event ON entry.E_id=event.E_id WHERE entry.date = ?");
+            statements.add(eventQuery);
+            
+            taskInsert = conn.prepareStatement("INSERT INTO task (E_id, status, priority) VALUES (?, ?, ?)");
             statements.add(taskInsert);
-
-            // Let's update some rows as well...
-            taskUpdate = conn.prepareStatement(
-                        "UPDATE task SET name=?, description=?, date=?, time=?, statys=?, priority=?  where name=?");
-            statements.add(taskUpdate);
-
             
+            taskUpdate = conn.prepareStatement("UPDATE task SET status=?, priority=?  where E_id=?");
+            statements.add(taskUpdate);       
+            
+            taskDelete = conn.prepareStatement("DELETE from task WHERE E_id=?");
+            statements.add(taskDelete);
+            
+            taskQuery = conn.prepareStatement("SELECT * FROM entry LEFT JOIN task ON entry.E_id=event.E_id");
 
-
-            /*
-               We select the rows and verify the results.
-             */
-            rs = s.executeQuery(
-                    "SELECT name, description, date, time, isAllDay, endTime, repeating FROM event ORDER BY date");
 
             /* 
              * Normally, it is best to use a pattern of
@@ -132,63 +115,6 @@ public class Derby
              * comprehend, so we use a different pattern.
              */
             
-            
-            
-            
-			/*
-               We commit the transaction. Any changes will be persisted to
-               the database now.
-             */
-            conn.commit();
-            System.out.println("Committed the transaction");
-
-            /*
-             * In embedded mode, an application should shut down the database.
-             * If the application fails to shut down the database,
-             * Derby will not perform a checkpoint when the JVM shuts down.
-             * This means that it will take longer to boot (connect to) the
-             * database the next time, because Derby needs to perform a recovery
-             * operation.
-             *
-             * It is also possible to shut down the Derby system/engine, which
-             * automatically shuts down all booted databases.
-             *
-             * Explicitly shutting down the database or the Derby engine with
-             * the connection URL is preferred. This style of shutdown will
-             * always throw an SQLException.
-             *
-             * Not shutting down when in a client environment, see method
-             * Javadoc.
-             */
-
-            if (framework.equals("embedded"))
-            {
-                try
-                {
-                    // the shutdown=true attribute shuts down Derby
-                    DriverManager.getConnection("jdbc:derby:;shutdown=true");
-
-                    // To shut down a specific database only, but keep the
-                    // engine running (for example for connecting to other
-                    // databases), specify a database in the connection URL:
-                    //DriverManager.getConnection("jdbc:derby:" + dbName + ";shutdown=true");
-                }
-                catch (SQLException se)
-                {
-                    if (( (se.getErrorCode() == 50000)
-                            && ("XJ015".equals(se.getSQLState()) ))) {
-                        // we got the expected exception
-                        System.out.println("Derby shut down normally");
-                        // Note that for single database shutdown, the expected
-                        // SQL state is "08006", and the error code is 45000.
-                    } else {
-                        // if the error code or SQLState is different, we have
-                        // an unexpected exception (shutdown failed)
-                        System.err.println("Derby did not shut down normally");
-                        printSQLException(se);
-                    }
-                }
-            }
         }
         catch (SQLException sqle)
         {
@@ -199,6 +125,22 @@ public class Derby
             handle();
             
         }
+    }
+    
+    private void createTables() { //If the tables already exist, this code block will NOT execute
+    	System.out.println("Creating Tables");
+    	try {
+    		s.execute("CREATE TABLE entry(E_id int NOT NULL AUTO_INCREMENT PRIMARY KEY, name varchar(255), description varchar(255), date varchar(255), time varchar(255)");
+            System.out.println("Created table entry");
+    	} catch (SQLException e) {}
+    	try {
+    		s.execute("CREATE TABLE event(E_id int NOT NULL PRIMARY KEY, isAllDay boolean, endTime varchar(255), repeating smallint)");
+            System.out.println("Created table event");
+    	} catch(SQLException e) {}
+    	try {
+    		s.execute("CREATE TABLE task(E_id int NOT NULL PRIMARY KEY, status smallint, priority smallint)");
+            System.out.println("Created table task");
+    	} catch (SQLException e) {}
     }
     
     private void handle() {
@@ -239,13 +181,20 @@ public class Derby
     
     public void addEvent(String name, String desc, String date, String time, boolean isAllDay, String endTime, int repeating) {
     	try {
-    		eventInsert.setString(1, name);
-            eventInsert.setString(2, desc);
-            eventInsert.setString(3, date);
-            eventInsert.setString(4, time);
-            eventInsert.setBoolean(5, isAllDay);
-            eventInsert.setString(6, endTime);
-            eventInsert.setInt(7, repeating);
+    		entryInsert.setString(1, name);
+    		entryInsert.setString(2, desc);
+    		entryInsert.setString(3, date);
+    		entryInsert.setString(4,time);
+    		entryInsert.executeUpdate();
+    		
+    		ResultSet id = entryInsert.getGeneratedKeys();
+    		
+    		long E_id = id.getLong(1); //Grab the Primary Key of the Entry to be used as a Foreign Key for Event
+    		
+    		eventInsert.setLong(1, E_id);
+            eventInsert.setBoolean(2, isAllDay);
+            eventInsert.setString(3, endTime);
+            eventInsert.setInt(4, repeating);
             eventInsert.executeUpdate();
     	} catch (SQLException e) {
     		printSQLException(e);
@@ -264,6 +213,34 @@ public class Derby
             eventUpdate.setString(6, endTime);
             eventUpdate.setInt(7, repeating);
             eventUpdate.setString(8, toReplace);
+            eventUpdate.executeUpdate();
+    	} catch(SQLException e) {
+    		printSQLException(e);
+    	} finally {
+    		handle();
+    	}
+    }
+    
+    public void deleteEvent(long E_id) {
+    	try {
+    		entryDelete.setLong(1, E_id);
+    		entryDelete.executeUpdate();
+    		
+    		eventDelete.setLong(1,E_id);
+    		eventDelete.executeUpdate();
+    		
+    	} catch(SQLException e) {
+    		printSQLException(e);
+    	} finally {
+    		handle();
+    	}
+    }
+    
+    public void queryEvent(String date) {
+    	try {
+    		eventQuery.setString(1, date);
+    		eventQuery.executeUpdate();
+    		
     	} catch(SQLException e) {
     		printSQLException(e);
     	} finally {
@@ -273,12 +250,20 @@ public class Derby
     
     public void addTask(String name, String desc, String date, String time, int status, int priority) {
     	try {
-    		taskUpdate.setString(1, name);
-            taskUpdate.setString(2, desc);
-            taskUpdate.setString(3, date);
-            taskUpdate.setString(4, time);
-            taskUpdate.setInt(5, status);
-            taskUpdate.setInt(6, priority);
+    		taskInsert.setString(1, name);
+    		taskInsert.setString(2, desc);
+    		taskInsert.setString(3, date);
+    		taskInsert.setString(4,time);
+    		taskInsert.executeUpdate();
+    		
+    		ResultSet id = entryInsert.getGeneratedKeys();
+    		
+    		long E_id = id.getLong(1); //Grab the Primary Key of the Entry to be used as a Foreign Key for Event
+    		
+    		taskInsert.setLong(1, E_id);
+            taskInsert.setInt(2, status);
+            taskInsert.setInt(3, priority);
+            eventInsert.executeUpdate();
     	} catch(SQLException e) {
     		printSQLException(e);
     	} finally {
@@ -295,6 +280,7 @@ public class Derby
             taskUpdate.setInt(5, status);
             taskUpdate.setInt(6, priority);
             taskUpdate.setString(7, toReplace);
+            taskUpdate.executeUpdate();
     	} catch(SQLException e) {
     		printSQLException(e);
     	} finally {
@@ -302,6 +288,93 @@ public class Derby
     	}
     }
 
+    public void deleteTask(long E_id) {
+    	try {
+    		entryDelete.setLong(1, E_id);
+    		entryDelete.executeUpdate();
+    		
+    		taskDelete.setLong(1,E_id);
+    		taskDelete.executeUpdate();
+    		
+    	} catch(SQLException e) {
+    		printSQLException(e);
+    	} finally {
+    		handle();
+    	}
+    }
+    
+    public void queryTask(String date) {
+    	try {
+    		taskQuery.setString(1, date);
+    		taskQuery.executeUpdate();
+    		
+    	} catch(SQLException e) {
+    		printSQLException(e);
+    	} finally {
+    		handle();
+    	}
+    }
+    
+    public void updateDatabase() {
+    	try {
+    		conn.commit();
+    	} catch (SQLException e) {
+    		printSQLException(e);
+    	} finally {
+    		handle();
+    	}
+    }
+
+    public void closeDatabase() {
+		/*
+         * In embedded mode, an application should shut down the database.
+         * If the application fails to shut down the database,
+         * Derby will not perform a checkpoint when the JVM shuts down.
+         * This means that it will take longer to boot (connect to) the
+         * database the next time, because Derby needs to perform a recovery
+         * operation.
+         *
+         * It is also possible to shut down the Derby system/engine, which
+         * automatically shuts down all booted databases.
+         *
+         * Explicitly shutting down the database or the Derby engine with
+         * the connection URL is preferred. This style of shutdown will
+         * always throw an SQLException.
+         *
+         * Not shutting down when in a client environment, see method
+         * Javadoc.
+         */
+    	
+        if (framework.equals("embedded"))
+        {
+            try
+            {
+                // the shutdown=true attribute shuts down Derby
+                DriverManager.getConnection("jdbc:derby:;shutdown=true");
+
+                // To shut down a specific database only, but keep the
+                // engine running (for example for connecting to other
+                // databases), specify a database in the connection URL:
+                //DriverManager.getConnection("jdbc:derby:" + dbName + ";shutdown=true");
+            }
+            catch (SQLException se)
+            {
+                if (( (se.getErrorCode() == 50000)
+                        && ("XJ015".equals(se.getSQLState()) ))) {
+                    // we got the expected exception
+                    System.out.println("Derby shut down normally");
+                    // Note that for single database shutdown, the expected
+                    // SQL state is "08006", and the error code is 45000.
+                } else {
+                    // if the error code or SQLState is different, we have
+                    // an unexpected exception (shutdown failed)
+                    System.err.println("Derby did not shut down normally");
+                    printSQLException(se);
+                }
+            }
+        }
+    }
+    
     //Loads the embedded driver
     private void loadDriver() {
         /*
@@ -361,17 +434,5 @@ public class Derby
             //e.printStackTrace(System.err);
             e = e.getNextException();
         }
-    }
-
-    private void parseArguments(String[] args)
-    {
-        if (args.length > 0) {
-            if (args[0].equalsIgnoreCase("derbyclient"))
-            {
-                framework = "derbyclient";
-                driver = "org.apache.derby.jdbc.ClientDriver";
-                protocol = "jdbc:derby://localhost:1527/";
-            }
-        }
-    }
+    }    
 }
