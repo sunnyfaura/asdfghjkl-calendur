564376f3975138f8632eb5a862ba2b318afd4e00
ocls <oscarclsilva@gmail.com>
2012-10-04 09:41:08 +0800

    wat do

---
+++ Entry.java
@@ -0,0 +1,17 @@
+import java.sql.Timestamp;
+
+public abstract class Entry
+{
+	public int id;
+	public String name;
+	public String desc;
+	public Timestamp time;
+	
+	protected Entry(int i, String n, String dsc, Timestamp t)
+	{
+		id = i;
+		name = n;
+		desc = dsc;
+		time = t;
+	}
+}
